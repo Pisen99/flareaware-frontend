512cb82d3332e324c6cd1c6a16b46b1b7d7c9b89
Patricia Samus <patricia.samus@gmail.com>
2025-06-04 19:33:36 +0200

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Button() {
+
+    return (
+        <div>
+            <button className="p-4 bg-amber-300">test</button>
+        </div>
+    )
+}
+
+export default Button;
