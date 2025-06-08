cb79255901dff158ea8dc965adf1878f3288f6a7
Patricia Samus <patricia.samus@gmail.com>
2025-06-08 22:24:11 +0200

    FEAT: Created type animation to hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../components/Button";
 import img from "../assets/img/flareaware-bg.png";
 import Icons from "./Icons";
+import { TypeAnimation } from "react-type-animation";
 
 function Hero() {
 
@@ -18,9 +19,21 @@ function Hero() {
                 <h1 className="text-4xl font-bold text-beige">
                     FlareAware
                 </h1>
-                <p className="text-xl text-beige">
-                    Track, Manage & Take Control
-                </p>
+                <TypeAnimation
+                sequence={[
+                    "Track",
+                    500,
+                    "Track, Manage",
+                    500,
+                    "Track, Manage & Take Control"
+                ]}
+                wrapper="span"
+                speed={20}
+                cursor={false}
+                className="text-xl text-beige"
+                repeat={0}
+                >
+                </TypeAnimation>
             </div>
             <div className="min-h-[50vh] flex flex-col items-center justify-end p-4 gap-4">
                 <p className="text-lg text-beige">Get Started</p>
