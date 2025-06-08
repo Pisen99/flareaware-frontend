ecda7cd13534fda5aff39c05a033060dc1f48a33
Patricia Samus <patricia.samus@gmail.com>
2025-06-08 19:53:57 +0200

    FEAT: Added background image (temporarily)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Button from "../components/Button";
+import img from "../assets/img/flareaware-bg.png";
 
 function Hero() {
 
     return (
         <>
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <div className="min-h-[50vh] flex flex-col items-center justify-end">
-                <h1>
+        <div className="min-h-screen flex flex-col items-center justify-center"
+            style={{
+                backgroundImage: `url(${img})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover" 
+            }}
+        >
+            <div className="min-h-[50vh] flex flex-col items-center justify-end gap-4">
+                <h1 className="text-4xl">
                     FlareAware
                 </h1>
-                <p>-</p>
-                <p>
+                <p className="text-xl">
                     Track, Manage & Take Control
                 </p>
             </div>
