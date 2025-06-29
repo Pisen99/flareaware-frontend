7f90177dfa514f929846d33101c0564dea515de4
Patricia Samus <patricia.samus@gmail.com>
2025-06-29 18:59:42 +0200

    FEAT: Implemented a function to handle clicked day

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import moment from "moment";
+import { useState } from "react";
 
 function Calendar() {
     const today = moment();
@@ -13,13 +14,25 @@ function Calendar() {
         }
     })
 
+    const [selectedCard, setSelectedCard] = useState()
+
+    const handleActiveCard = (index) => {
+        console.log(index)
+        setSelectedCard(index)
+    }
+
     return (
         <>
             <div className="grid grid-flow-col grid-cols-7 text-center">
                 {thisWeek.map((item, index) => (
-                    <div key={index}>
+                    <div
+                        key={index}
+                    >
                         <h2>{item.month}</h2>
-                        <div className={`bg-blue-400 border-2 rounded-full p-2 ${item.currentDay ? "border-blue-600" : "border-transparent"}`}>
+                        <div
+                            onClick={() => handleActiveCard(index)}
+                            className={`border-2 rounded-full p-2 ${item.currentDay ? "border-blue-600" : "border-transparent"} ${selectedCard === index ? "bg-blue-300" : "bg-blue-400"}`}
+                        >
                             <p>{item.day}</p>
                             <p>{item.date}</p>
                         </div>
