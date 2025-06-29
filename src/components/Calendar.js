7d3d2f48f43bd4d81cebd53adfc359247e1ff727
Patricia Samus <patricia.samus@gmail.com>
2025-06-29 18:35:21 +0200

    FEAT: Installed moment to use for date system. STYLE: Border to be implemented for active day

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,23 +1,27 @@
 import React from "react";
+import moment from "moment";
 
 function Calendar() {
-    const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-    const calCards = days.map((day) => ({
-        day: day,
-        date: ""
-    }));
+    const today = moment();
+    const thisWeek = Array.from({ length: 7 }, (_, i) => {
+        const day = moment().startOf('day').add(i, 'days');
+        return {
+            day: day.format("ddd")[0], // e.g., Mon
+            date: day.date(), // e.g., 28
+            month: day.format("MMM"), // e.g., Jun
+            currentDay: day.isSame(today, "day")
+        }
+    })
 
     return (
         <>
             <div className="grid grid-flow-col grid-cols-7 text-center">
-                {calCards.map((item, index) => (
-                    <div
-                        key={index}
-                    >
-                        <div className="bg-blue-400 rounded-full p-2">
+                {thisWeek.map((item, index) => (
+                    <div key={index}>
+                        <h2>{item.month}</h2>
+                        <div className={`bg-blue-400 border-2 rounded-full p-2 ${item.currentDay ? "border-blue-600" : "border-transparent"}`}>
                             <p>{item.day}</p>
-                            {item.date}
+                            <p>{item.date}</p>
                         </div>
                     </div>
                 ))}
