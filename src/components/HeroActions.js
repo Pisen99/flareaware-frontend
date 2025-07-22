31e8b3b77ce0886a419c122e0f7495db31f08c73
Patricia Samus <patricia.samus@gmail.com>
2025-07-23 00:24:11 +0200

    UPD: added more properties to Buttons.

--- src/components/HeroActions.js
+++ src/components/HeroActions.js
@@ -6,16 +6,21 @@ function HeroActions() {
     
     return (
         <div className="flex flex-row gap-x-4 justify-center whitespace-nowrap">
+            {/* Get started */}
             <Button
-                label={"Get Started"}
                 variant="secondary"
+                label="Get Started"
                 onClick={() => console.log("Get Started")}
             />
+            {/* Learn more */}
             <Button
-                label={"Learn More"}
                 variant="primary"
+                symbol={Icons.symbols.info}
+                label="Learn More"
+                expand={true}
                 onClick={() => console.log("Learn More")}
             />
+            
         </div>
     )
 }
