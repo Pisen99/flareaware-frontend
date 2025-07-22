ae8c0507e7001824baeb0692e060aa77542909eb
Patricia Samus <patricia.samus@gmail.com>
2025-07-23 00:21:04 +0200

    FEAT: Created an 'expand' option for button, inst => clsx for multiline code. FIX: Cleaned up code, var names changes, style changes.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,58 +1,92 @@
 import React from "react";
+import clsx from "clsx";
 
-function Button({ label, variant, onClick, className = "" }) {
-    // DEFAULT btn syle
-    const btnDefault = `
-        text-beige
-        relative w-full sm:w-auto
+function Button({
+    label,
+    variant = "primary",
+    symbol,
+    onClick,
+    className = "",
+    expand = false,
+}) {
+
+    const btnBase = `
+        flex justify-center items-center
+        relative w-auto
         px-4 py-2
-        rounded-full font-semibold text-center
+
+        bg-gray-600 border-2 border-beige
+
+        text-beige font-semibold text-center
+        text-sm md:text-xl
+        rounded-full 
+
         transition
-        overflow-hidden group
-        cursor-pointer
+        overflow-hidden
+        group cursor-pointer
 
-        text-sm
-        md:text-xl
-    `.trim().replace(/\s+/g, ' ');
+        focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-300
 
-    // VARIANTS btn style
-    const variants = {
-        primary: `
+    `;
+
+    const btnVariant = {
+        primary: clsx(`
             bg-gray-800
             text-gray-600
-            backdrop-blur-md
-            shadow-md shadow-gray-600/50 
-            border-gray-600 border-2
-            duration-300 ease-in-out
+            shadow-gray-600/50 shadow-md 
+            border-gray-600
 
-            hover:bg-gray-700/80
+            backdrop-blur-md
+            duration-300 ease-in-out
+            
             hover:shadow-md
             
-            active:inset-shadow-sm active:inset-shadow-gray-900 active:shadow-xs active:scale-95
-        `.trim().replace(/\s+/g, ' '),
-        secondary: `
+            active:inset-shadow-gray-900 active:inset-shadow-sm
+            active:shadow-xs
+            active:scale-95
+        `),
+        secondary: clsx(`
             bg-green/30
             text-green
+            shadow-green/50 shadow-md 
+            border-green
+
             backdrop-blur-md
-            shadow-md shadow-green/50
-            border-green border-2
             duration-300 ease-in-out
 
             hover:bg-green/60
             hover:shadow-md
             hover:-translate-y-1 hover:scale-100
             
-            active:inset-shadow-sm active:inset-shadow-gray-900 active:shadow-xs active:scale-95
-        `.trim().replace(/\s+/g, ' '),
-        icon: "text-2xl md:text-3xl lg:text-4xl",
+            active:inset-shadow-gray-900 active:inset-shadow-sm
+            active:shadow-xs
+            active:scale-95
+        `),
+        icon: clsx(`
+            bg-transparent border-transparent
+            !text-2xl !md:text-3xl !lg:text-4xl
+        `),
     };
 
-    const btnClasses = `${btnDefault} ${variants[variant] || ""} ${className}`.trim();
+    const btnClasses = clsx(
+        btnBase,
+        btnVariant[variant],
+        className
+    );
 
     return (
-        <div>
-            <button onClick={onClick} className={btnClasses}>{label}</button>
-        </div>
+        <button onClick={onClick} className={btnClasses}>
+            {symbol && <span className="transition-all duration-500">{symbol}</span>}
+            {label && (
+                <span className={clsx(
+                    "transition-all duration-500 whitespace-nowrap",
+                    expand ? "max-w-0 opacity-0 overflow-hidden group-hover:ml-2 group-hover:max-w-[200px] group-hover:opacity-100 transform translate-x-[-8px] group-hover:translate-x-0" : "opacity-100 max-w-full"
+                )}
+                >
+                    {label}
+                </span>
+            )}
+        </button>
     )
 };
 
