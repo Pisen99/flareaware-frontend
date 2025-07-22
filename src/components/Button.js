73bfa707fbf250c787d63ef49861f31491acce09
Patricia Samus <patricia.samus@gmail.com>
2025-07-22 21:27:14 +0200

    UPD: colors to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -28,21 +28,21 @@ function Button({ label, variant, onClick, className = "" }) {
             hover:bg-gray-700/80
             hover:shadow-md
             
-            active:inset-shadow-sm active:inset-shadow-gray-900 active:shadow-xs
+            active:inset-shadow-sm active:inset-shadow-gray-900 active:shadow-xs active:scale-95
         `.trim().replace(/\s+/g, ' '),
         secondary: `
-            bg-violet-600/30
-            text-violet-500
+            bg-green/30
+            text-green
             backdrop-blur-md
-            shadow-md shadow-violet-500/50 
-            border-violet-500 border-2
+            shadow-md shadow-green/50
+            border-green border-2
             duration-300 ease-in-out
 
-            hover:bg-violet-700/60
+            hover:bg-green/60
             hover:shadow-md
-            hover:-translate-y-1 hover:scale-110
+            hover:-translate-y-1 hover:scale-100
             
-            active:inset-shadow-sm active:inset-shadow-gray-900 active:shadow-xs
+            active:inset-shadow-sm active:inset-shadow-gray-900 active:shadow-xs active:scale-95
         `.trim().replace(/\s+/g, ' '),
         icon: "text-2xl md:text-3xl lg:text-4xl",
     };
