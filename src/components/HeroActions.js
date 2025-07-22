2d8a1c17ca133deafdd6e30cf1f4056f0fd64a66
Patricia Samus <patricia.samus@gmail.com>
2025-07-23 00:27:25 +0200

    FIX: order of properties => HeroActions

--- src/components/HeroActions.js
+++ src/components/HeroActions.js
@@ -8,15 +8,15 @@ function HeroActions() {
         <div className="flex flex-row gap-x-4 justify-center whitespace-nowrap">
             {/* Get started */}
             <Button
-                variant="secondary"
                 label="Get Started"
+                variant="secondary"
                 onClick={() => console.log("Get Started")}
             />
             {/* Learn more */}
             <Button
-                variant="primary"
-                symbol={Icons.symbols.info}
                 label="Learn More"
+                symbol={Icons.symbols.info}
+                variant="primary"
                 expand={true}
                 onClick={() => console.log("Learn More")}
             />
